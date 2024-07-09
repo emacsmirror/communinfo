30766103f2e130c971bd479e7fc1bbe5854f0baf
Mekeor Melire <mekeor@posteo.de>
2024-07-09 11:12:28 +0200

    commentary: mention correct variable name

--- communinfo.el
+++ communinfo.el
@@ -30,10 +30,10 @@
 ;; Version 30 of GNU Emacs introduces a user option variable
 ;; `Info-url-alist' that allows to associate Info manuals to URLs.
 ;; Its default value only associates those Info manuals that are part
-;; of Emacs.  This package provides the variable `communinfo' which
-;; can be used as value for `Info-url-alist' that provides
-;; URL-associations for many more free, libre and open source software
-;; projects.
+;; of Emacs.  This package provides the variable
+;; `communinfo-url-alist' which can be used as value for
+;; `Info-url-alist' that provides URL-associations for many more free,
+;; libre and open source software projects.
 
 ;; Here's how to use `communinfo' after installation:
 ;;
